bbfe1c94cfb45f266cc75d08aaa64f1f6106217f
Gurupranav-tech <67361835+Gurupranav-tech@users.noreply.github.com>
2024-09-27 14:30:46 +0530

    Added error boundaries for the invoices page

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Acme Website</title>
+      </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
